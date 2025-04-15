f1da625786a4864a3011178f80ca901a29abb2c5
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-04-15 14:27:00 -0400

    fixed version mismatch

--- script/Trigger.s.sol
+++ script/Trigger.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity ^0.8.19;
 
 import {RewardsDistributor} from "contracts/RewardsDistributor.sol";
 import {ITypes} from "interfaces/ITypes.sol";
--- src/contracts/RewardsDistributor.sol
+++ src/contracts/RewardsDistributor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity ^0.8.19;
 
 import {ISimpleTrigger} from "interfaces/IWavsTrigger.sol";
 import {ISimpleSubmit} from "interfaces/IWavsSubmit.sol";
